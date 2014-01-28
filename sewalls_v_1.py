ed679a7b2ded7e6146cb1169104e90921bbd4160
sopier <sopier@gmail.com>
2014-01-28 12:54:56 +0700

    asdasda

--- sewalls_v_1.py
+++ sewalls_v_1.py
@@ -11,3 +11,12 @@ soup = BeautifulSoup(html)
 divs = soup.findAll("div", attrs={"class": "grid_6"})
 # got single link
 links = [i.find("a", href=True)["href"] for i in divs]
+
+html = urllib2.urlopen(links[0]).read()
+soup = BeautifulSoup(html)
+
+for link in links:
+    html = urllib2.urlopen(link).read()
+    soup = BeautifulSoup(html)
+    hdpost = soup.find("div", attrs={"class": "hdpost"})
+    print(hdpost.find("a", href=True)["href"])
