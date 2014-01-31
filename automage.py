2857c2489298650df6b31584b507a50051491908
sopier <sopier@gmail.com>
2014-01-31 22:41:24 +0700

    first commit

---
+++ automage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+1. create thumbnail
+2. insert database
+{
+title: ...,
+filesize: ...,
+format: ...,
+added: ...,
+dims: ...,
+hits: ...,
+tags: ...,
+}
+"""
+import os
+from PIL import Image
+
+def create_thumb(image):
+    pass
+
+def insert_dbase(image):
+    pass
+
+def move_thumb(thumb):
+    pass
+
+if __name__ == "__main__":
+    create_thumb(image)
+    insert_dbase(image)
+    move_thumb(image)
