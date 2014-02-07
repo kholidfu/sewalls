00be3dc7ab4f0767c5211585ac7893f237bb33b7
sopier <sopier@gmail.com>
2014-02-07 21:38:30 +0700

    code edit

--- img_crawler_pichost.py
+++ img_crawler_pichost.py
@@ -44,43 +44,31 @@ def phostgrab(url):
         filetype = response.info()["Content-Type"].split("/")[-1]
         filesize = response.info()["Content-Length"]
 
-        #print average_hash(Image.open(StringIO(response.read()))).__str__()
-        # pastikan dulu bahwa filetype itu image
-        # if blabla
-        #with open("/home/banteng/Desktop/pichost/" + h1 + "_" + imgurl.split("/")[-1].split(".")[0] + "." + filetype, "wb") as f:
-        #f.write(response.read())
-
-        try:
-            buf = StringIO(response.read())
-            im = Image.open(buf)
-        except:
-            print 'error opening image'
+        buf = StringIO(response.read())
+        im = Image.open(buf)
 
         if im.size[0] >= 1920 and im.size[0]/float(im.size[1]) >= 1.6:
             try:
-
-
                 t.resize_and_crop(
-                    StringIO(urllib2.urlopen(imgurl).read()),
-                    "/home/banteng/Desktop/thumb_" + h1 + filetype,
+                    StringIO(urllib2.urlopen(imgurl).read()), # PR besar, kenapa response harus di-load 2x? :(
+                    "/home/banteng/Desktop/thumb_" + h1 + "." + filetype,
                     (252, 188),
                     'middle')
+                # insert into mongodb only for qualified image
+                db2.wallpaper.insert({
+                    'title': h1, # full-text search
+                    'url': url, # unique
+                    'format': filetype,
+                    'size': filesize,
+                    'added': datetime.now(),
+                    'hits': 0,
+                    'tags': h1.split() + [urlparse(url).hostname],
+                    'imghash': hashlib.md5(response.read()).hexdigest(),
+                })
+                sys.stdout.write("sukses inserting data\n")
+
             except IOError as e:
                 print e
-
-        # insert into mongodb
-        db2.wallpaper.insert({
-            'title': h1, # full-text search
-            'url': url, # unique
-            'format': filetype,
-            'size': filesize,
-            'added': datetime.now(),
-            'hits': 0,
-            'tags': h1.split() + [urlparse(url).hostname],
-             'imghash': hashlib.md5(response.read()).hexdigest(),
-            })
-
-        sys.stdout.write("sukses inserting data\n")
 
     except:
         pass
@@ -102,30 +90,4 @@ for url in urls:
 # updating data status back to 0
 #>>> [db.url.update({'status': 1}, {"$set": {"status": 0}}) for i in db.url.find({'status': 1})]
 
-# setelah itu thumbnail
-# @TODO:
-# auto generate folder when 10.000 images limit reached
-# how to count images on each folder?
-
-# for image in os.listdir("/home/banteng/Desktop/pichost"):
-#     try:
-#         im = Image.open("/home/banteng/Desktop/pichost/" + image)
-#         sys.stdout.write("opening image " + image + "\n")
-#         if im.size[0] >= 1920 and im.size[0]/float(im.size[1]) >= 1.6:
-#             t.resize_and_crop(
-#                 "/home/banteng/Desktop/pichost/" + image,
-#                 "/home/banteng/Desktop/thumb/thumb_" + image,
-#                 (252, 188),
-#                 'middle')
-#             sys.stdout.write("thumbnailing image " + image + "\n")
-#     except:
-#         sys.stdout.write("gagal\n")
-#         continue
-#
-#
-#
-# for image in os.listdir("/home/banteng/Desktop/pichost"):
-#     os.unlink("/home/banteng/Desktop/pichost/" + image)
-#
-# sys.stdout.write("job all done!\n")
 # done!
